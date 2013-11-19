4e4cbf7bf63fdefa21134ae7fa51a7aa67450fa1
Warolf <jesusrdiogo@gmail.com>
2013-11-19 17:11:29 +0000

    Revert "Compont List Aniews Joomla 3.2"
    This reverts commit 40e3a2a97d6703c79aa039dd6da785050e01f84c.

--- assets/highlight.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-    $('#jform_company_id').change(function() {
-        //document.adminForm.submit();
-        window.location = window.location+'&company_id='+$('#jform_company_id')[0].value;
-    });
-});
